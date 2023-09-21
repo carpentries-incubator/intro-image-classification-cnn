0fa975834c2c476e59c8751406559c3926e2330f
Jason Bell <j.bell@cqu.edu.au>
2023-09-21 14:13:49 +1000

    Jason Bell - created test-setup.py file that users can use to test everything is installed

---
+++ episodes/scripts/test-setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 14:10:18 2023
+
+@author: Jason Bell
+"""
+
+import sklearn
+print('sklearn version: ', sklearn.__version__)
+
+import seaborn
+print('seaborn version: ', seaborn.__version__)
+
+import pandas
+print('pandas version: ', pandas.__version__)
+
+import tensorflow
+print('Tensorflow version: ', tensorflow.__version__)

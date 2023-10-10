f8a00ff68df0800bb11172cd12954499b9d561f2
Vlad Dracula <erin.graham@jcu.edu.au>
2023-10-10 16:24:29 +1000

    finish content changes re prediction conf matrix; update figs; fix code to match

--- episodes/scripts/predict.py
+++ episodes/scripts/predict.py
@@ -8,7 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # load weights for your best model if not still in memory
-model_dropout = keras.models.load_model('C:/Users/jc140298/Documents/QCIF/SwC/20230316_ML_AI/scripts/outputs_cinic10/model_dropout.h5')
+model_dropout = keras.models.load_model('C:/Users/jc140298/Documents/Software_Carpentry/20230316_ML_AI/scripts/outputs_cinic10/model_dropout.h5')
 
 # recreate test_images from 'image-data.py' if not still in memory
 
@@ -18,15 +18,13 @@ print('We are using ', model_dropout.name, '\n')
 # check test image dataset is loaded
 print('The test image dataset has the shape: ', test_images.shape)
 
-# predict the class labels
-result = model_dropout.predict(test_images)
-
 # use our current best model to predict probability of each class on new test set
 predicted_prob = model_dropout.predict(test_images)
 
 # create a dictionary to convert from string labels to numeric labels
-label_str_int_map = {'airplane': 0, 'automobile': 1, 'bird': 2, 'cat': 3, 'deer': 4, 'dog': 5, 'frog': 6, 'horse': 7, 'ship': 8, 'truck': 9}
+#label_str_int_map = {'airplane': 0, 'automobile': 1, 'bird': 2, 'cat': 3, 'deer': 4, 'dog': 5, 'frog': 6, 'horse': 7, 'ship': 8, 'truck': 9}
 #TODO 
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 # convert probability predictions to table using class names for column names
 prediction_df = pd.DataFrame(predicted_prob, columns=class_names)

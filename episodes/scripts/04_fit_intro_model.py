777a60964f0971c1ee9fc4a4e7293dfc9fcbe8f0
Vlad Dracula <erin.graham@jcu.edu.au>
2024-05-11 12:41:25 +1000

    add method def for compile, fit, replace shorthand

---
+++ episodes/scripts/04_fit_intro_model.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Image Classification with Convolutional Neural Networks
+
+Episode 04 Compile and Train (Fit) a Convolutional Neural Network
+
+"""
+
+#%%
+
+# load the required packages
+
+from tensorflow import keras # data and neural network
+from sklearn.model_selection import train_test_split # data splitting
+import matplotlib.pyplot as plt # plotting
+import seaborn as sns # specialised plotting
+import pandas as pd # handles dataframes
+import time # track run time
+
+#%%
+
+# start timer
+start = time.time()
+
+#%%
+
+# function to prepare the training dataset
+
+def prepare_dataset(train_images, train_labels):
+    
+    # normalize the RGB values to be between 0 and 1
+    train_images = train_images / 255
+    
+    # one hot encode the training labels
+    train_labels = keras.utils.to_categorical(train_labels, len(class_names))
+    
+    # split the training data into training and validation set
+    train_images, val_images, train_labels, val_labels = train_test_split(
+    train_images, train_labels, test_size = 0.2, random_state=42)
+
+    return train_images, val_images, train_labels, val_labels
+
+#%%
+
+# function to define the introduction model
+
+def create_model_intro():
+    
+    # CNN Part 1
+    # Input layer of 32x32 images with three channels (RGB)
+    inputs_intro = keras.Input(shape=train_images.shape[1:])
+    
+    # CNN Part 2
+    # Convolutional layer with 16 filters, 3x3 kernel size, and ReLU activation
+    x_intro = keras.layers.Conv2D(filters=16, kernel_size=(3,3), activation='relu')(inputs_intro)
+    # Pooling layer with input window sized 2x2
+    x_intro = keras.layers.MaxPooling2D(pool_size=(2,2))(x_intro)
+    # Second Convolutional layer with 32 filters, 3x3 kernel size, and ReLU activation
+    x_intro = keras.layers.Conv2D(filters=32, kernel_size=(3,3), activation='relu')(x_intro)
+    # Second Pooling layer with input window sized 2x2
+    x_intro = keras.layers.MaxPooling2D(pool_size=(2,2))(x_intro)
+    # Flatten layer to convert 2D feature maps into a 1D vector
+    x_intro = keras.layers.Flatten()(x_intro)
+    # Dense layer with 64 neurons and ReLU activation
+    x_intro = keras.layers.Dense(units=64, activation='relu')(x_intro)
+    
+    # CNN Part 3
+    # Output layer with 10 units (one for each class) and softmax activation
+    outputs_intro = keras.layers.Dense(units=10, activation='softmax')(x_intro)
+    
+    # create the model
+    model_intro = keras.Model(inputs = inputs_intro, 
+                              outputs = outputs_intro, 
+                              name = "cifar_model_intro")
+    
+    return model_intro
+
+#%%
+
+# load the data
+(train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
+
+# create a list of classnames
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+# prepare the dataset for training
+train_images, val_images, train_labels, val_labels = prepare_dataset(train_images, train_labels)
+
+#%%
+
+# create the introduction model
+model_intro = create_model_intro()
+
+# view model summary
+model_intro.summary()
+
+#%%
+
+### Step 5. Choose a loss function and optimizer and compile model
+
+## CHALLENGE Write the code to compile the introductory model
+
+# compile the model
+#hidden#.compile(optimizer = #hidden#, 
+                    loss = #hidden#, 
+                    metrics = #hidden#)
+                    
+#%%
+                                    
+### Step 6. Train (Fit) model
+
+# fit the model
+#hidden# = #hidden#.fit(x = #hidden#, y = #hidden#,
+                                batch_size = #hidden#,
+                                epochs = #hidden#, 
+                                validation_data = (#hidden#, #hidden#))
+
+#%%
+# save the model
+model_intro.save('fit_outputs/model_intro.keras')
+
+#%%
+
+# Monitor Training Progress (aka Model Evaluation during Training)
+
+# convert the model history to a dataframe for plotting 
+history_intro_df = pd.DataFrame.from_dict(history_intro.history)
+
+# plot the loss and accuracy from the training process
+fig, axes = plt.subplots(1, 2)
+fig.suptitle('cifar_model_intro')
+sns.lineplot(ax=axes[0], data=history_intro_df[['loss', 'val_loss']])
+sns.lineplot(ax=axes[1], data=history_intro_df[['categorical_accuracy', 'val_categorical_accuracy']])
+
+
+#%%
+
+end = time.time()
+
+print()
+print()
+print("Time taken to run program was:", end - start, "seconds")
